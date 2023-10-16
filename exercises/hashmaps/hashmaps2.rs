c1fe75c35ec234d7eadbab2d56ab36bf1aeb2c06
Pirate <2311566266@qq.com>
2023-10-16 20:59:07 +0800

    update

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -35,6 +33,9 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         Fruit::Lychee,
         Fruit::Pineapple,
     ];
+
+    basket.insert(Fruit::Banana, 10);
+    basket.insert(Fruit::Pineapple, 20);
 
     for fruit in fruit_kinds {
         // TODO: Insert new fruits if they are not already present in the
@@ -81,7 +82,7 @@ mod tests {
         let count = basket.values().sum::<u32>();
         assert!(count > 11);
     }
-    
+
     #[test]
     fn all_fruit_types_in_basket() {
         let mut basket = get_fruit_basket();
